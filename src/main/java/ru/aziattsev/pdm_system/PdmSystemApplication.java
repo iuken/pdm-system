abde495ec0e168d0d8a7140ed91b9e3d03528d07
iuken <iuken067@gmail.com>
2025-04-21 17:13:11 +0400

    feat: init commit

---
+++ src/main/java/ru/aziattsev/pdm_system/PdmSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.aziattsev.pdm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdmSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aziattsev/pdm_system/PdmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aziattsev.pdm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
